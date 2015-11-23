b2141095130fcf5b6d5def9f1d0f14a7e184ee67
Walter Binder <walter.binder@gmx.at>
2015-11-23 11:00:45 +0100

    add description and better presentation in main

--- Übung 1/bsp2_walter/main_kmean.m
+++ Übung 1/bsp2_walter/main_kmean.m
@@ -1,35 +1,42 @@
 %test kmean function with different inputs
 
-%test1 - simple.png , dimension = 5
+%### dimension = 5 ,fixed k = 5 
+k=5;
+%test1 - simple.png 
 % kmeans(input_image, dimension, k, threshold )
 test1_input_image=imread('images/simple.png');
-test1_image = kmeans(test1_input_image,5,5,1.01);
+test1_image = kmeans(test1_input_image,5,k,1.01);
 
-%test2 - future.jpg , dimension = 5
+%test2 - future.jpg 
 test2_input_image=imread('images/future.jpg');
-test2_image = kmeans(test2_input_image,5,3,1.01);
+test2_image = kmeans(test2_input_image,5,k,1.01);
 
-%test3 - mm.jpg , dimension = 5
+%test3 - mm.jpg
 test3_input_image=imread('images/mm.jpg');
-test3_image = kmeans(test3_input_image,5,3,1.01);
+test3_image = kmeans(test3_input_image,5,k,1.01);
 
-%test4 - simple.png , dimension = 3
+%### dimension = 3 ,fixed k = 5 
+
+%test4 - simple.png 
 test4_input_image=imread('images/simple.png');
-test4_image = kmeans(test1_input_image,3,3,1.01);
+test4_image = kmeans(test1_input_image,3,k,1.01);
 
 %test5 - future.jpg , dimension = 3
 test5_input_image=imread('images/future.jpg');
-test5_image = kmeans(test2_input_image,3,3,1.01);
+test5_image = kmeans(test2_input_image,3,k,1.01);
 
 %test6 - mm.jpg , dimension = 3
 test6_input_image=imread('images/mm.jpg');
-test6_image = kmeans(test3_input_image,3,3,1.01);
+test6_image = kmeans(test3_input_image,3,k,1.01);
 
+%###
 
 %print original image and new colored image
 figure();
 subplot(1,2,1); imshow(test1_input_image);
-subplot(1,2,2); imshow(test1_image);
+title='original simple.png k=5';
+subplot(1,2,2); imshow(test1_image);   
+title='result simple.png d = 5 k=5';
 
 figure();
 subplot(1,2,1); imshow(test2_input_image);
